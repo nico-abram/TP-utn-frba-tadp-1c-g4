c2dde45046e37552940fc05e8e690a272a312739
Nicolas <abramlujan@gmail.com>
2018-05-15 16:48:32 -0300

    Refactorizar tests

--- spec/traits_spec.rb
+++ spec/traits_spec.rb
@@ -1,5 +1,28 @@
 require_relative "../traits"
 
+def helperGeneric2(mensaje1, value1, value2, mensaje2, value3, value4, t1 = :T1, t2 = :T2)
+	Trait.define do name t1
+		method mensaje1 do
+			value1
+		end
+		method mensaje2 do
+			value3
+		end
+	end
+	Trait.define do name t2
+		method mensaje1 do
+			value2
+		end
+		if value4 then
+			method mensaje2 do
+				value4
+			end
+		end
+	end
+end
+def helperGeneric(mensaje, value1, value2, t1 = :T1, t2 = :T2)
+	helperGeneric2(mensaje, value1, value2, :nada, 0, 1, t1, t2)
+end
 describe Trait do
 	it "Uso normal y simple de un trait" do
 		Trait.define do
@@ -15,21 +38,7 @@ describe Trait do
 	end
 
 	it "Restar un metodo a un trait" do
-		Trait.define do
-			name :A
-			method :conflictingMethod do 
-				1
-			end
-			method :anotherMethod do 
-				"Hi"
-			end
-		end
-		Trait.define do
-			name :B
-			method :conflictingMethod do 
-				"Works"
-			end
-		end
+		helperGeneric2(:conflictingMethod, 1, "Works", :anotherMethod, "Hi", nil, :A, :B)
 		class SimpleTest
 			uses B + (A - :conflictingMethod)
 		end
@@ -38,15 +47,7 @@ describe Trait do
 	end
 
   	it "Uso alias simple" do
-		Trait.define do
-			name :A
-			method :metodo1 do
-				"hi"
-			end
-			method :metodo2 do
-				"bye"
-			end
-		end
+		helperGeneric2(:metodo1 , "hi", nil, :metodo2, "bye", nil, :A, :Nada)
 		class SimpleTest
 			uses A << :metodo1 > :hello
 		end
@@ -54,15 +55,7 @@ describe Trait do
 	end
 
 	it "Uso alias simple con sintaxis alternative" do
-		Trait.define do
-			name :A
-			method :metodo1 do
-				"hi"
-			end
-			method :metodo2 do
-				"bye"
-			end
-		end
+		helperGeneric2(:metodo1 , "hi", nil, :metodo2, "bye", nil, :A, :Nada)
 		class SimpleTest
 			uses A << (:metodo1 >> :hello)
 		end
@@ -70,19 +63,7 @@ describe Trait do
 	end
 
 	it "Multiples Alias" do
-		Trait.define do
-			name :A
-			method :h do
-				"hi"
-			end
-		end
-		puts 
-		Trait.define do
-			name :A
-			method :metodo1 do
-				"hi"
-			end
-		end
+		helperGeneric2(:metodo1 , "hi", nil, :metodo2, "bye", nil, :A, :Nada)
 		class SimpleTest
 			uses (A << :metodo1 > :metodo2) << :metodo2 > :metodo3
 		end
@@ -90,19 +71,7 @@ describe Trait do
 	end
 
 	it "Multiples Alias con sintaxis alternativa" do
-		Trait.define do
-			name :A
-			method :h do
-				"hi"
-			end
-		end
-		puts 
-		Trait.define do
-			name :A
-			method :metodo1 do
-				"hi"
-			end
-		end
+		helperGeneric2(:metodo1 , "hi", nil, :metodo2, "bye", nil, :A, :Nada)
 		class SimpleTest
 			uses (A << (:metodo1 > :metodo2)) << (:metodo2 > :metodo3)
 		end
@@ -110,18 +79,7 @@ describe Trait do
 	end
 	
 	it "Excepcion cuando hay un conflicto" do
-		Trait.define do
-			name :A
-			method :conflictingMethod do 
-				1
-			end
-		end
-		Trait.define do
-			name :B
-			method :conflictingMethod do 
-				"Works"
-			end
-		end
+		helperGeneric(:conflictingMethod, 1, "Worls", :A, :B)
 		class SimpleTest
 			uses A + B
 		end
@@ -134,94 +92,44 @@ describe Trait do
 
 	it "Estrategia de ejecutar todos los métodos" do
 		miValor = 2
-
-		Trait.define do
-			name :T1
+		Trait.define do name :T1
 			method :random_operation do
 				miValor += 2
 			end
 		end
-		Trait.define do
-			name :T2
+		Trait.define do name :T2
 			method :random_operation do
 				miValor *= 2
 			end
 		end
-
 		class SimpleTest
 			uses (T1.con_todos T2)
 		end
-
-		miTest = SimpleTest.new
-		miTest.random_operation()
-				
-		expect(miValor).to eq 8
+		expect(SimpleTest.new.random_operation()).to eq 8
 	end
 
 	it "Estrategia de ejecutar con fold" do
-		Trait.define do
-			name :T1
-			method :random_operation do
-				7
-			end
-		end
-		Trait.define do
-			name :T2
-			method :random_operation do
-				3
-			end
-		end
-
+		helperGeneric(:random_operation, 7, 3)
 		class SimpleTest
 			uses (T1.foldeando T2 do |res_1, res_2|
 					res_1 + res_2
 				end)
 		end
-
-		miTest = SimpleTest.new
-				
-		expect(miTest.random_operation()).to eq 10
+		expect(SimpleTest.new.random_operation()).to eq 10
 	end
 
 	it "Estrategia de condición de corte" do
-		Trait.define do
-			name :T1
-			method :random_operation do
-				7
-			end
-		end
-		Trait.define do
-			name :T2
-			method :random_operation do
-				8
-			end
-		end
-
+		helperGeneric(:random_operation, 7, 8)
 		class SimpleTest
 			uses (T1.con_corte T2 do |res|
 					res.odd?
 				end)
 		end
-
-		miTest = SimpleTest.new
-				
-		expect(miTest.random_operation()).to eq 7
+		expect(SimpleTest.new.random_operation()).to eq 7
 	end
 
 	it "Definir propia estrategia (ejecutar segundo método)" do
-		Trait.define do
-			name :T1
-			method :getSomeNum do
-				6
-			end
-		end
-		Trait.define do
-			name :T2
-			method :getSomeNum do
-				4
-			end
-		end
-		
+		helperGeneric(:getSomeNum, 6, 4)
 		Trait.define_strategy :exec_second do |p1, p2|
 			Proc.new { |*args|
 				p2.call(args)
@@ -232,81 +140,33 @@ describe Trait do
 					p1.call(args)
 				}
 			}
-
 		class SimpleTest
 			uses T1.exec_second T2
 		end
-		miTest = SimpleTest.new
-		expect(miTest.getSomeNum).to eq 4
-
+		expect(SimpleTest.new.getSomeNum).to eq 4
 		class SimpleTest2
 			uses T1.exec_fst T2
 		end
-		miTest2 = SimpleTest2.new
-		expect(miTest2.getSomeNum).to eq 6
+		expect(SimpleTest2.new.getSomeNum).to eq 6
 	end
 	  
 	it "Estrategia por metodo" do
-		Trait.define do
-			name :T1
-			method :getSomeNum do
-				6
-			end
-			method :getAnotherNum do
-				5
-			end
-		end
-		Trait.define do
-			name :T2
-			method :getSomeNum do
-				4
-			end
-			method :getAnotherNum do
-				3
-			end
-		end
-
+		helperGeneric2(:getSomeNum, 6, 4, :getAnotherNum, 5, 3)
 		class SimpleTest
 			uses T1.solucionar_con(:getSomeNum, :estrategia_izq) + T2
 		end
-
-		miTest = SimpleTest.new
-
-		expect(miTest.getSomeNum).to eq 6
+		expect(SimpleTest.new.getSomeNum).to eq 6
 		expect{SimpleTest.new.getAnotherNum}.to raise_error "Unresolved trait method conflict"
 	end
 
 	it "Estrategias definidas en el momento" do
-		Trait.define do
-			name :T1
-			method :getSomeNum do
-				6
-			end
-			method :getAnotherNum do
-				5
-			end
-		end
-		Trait.define do
-			name :T2
-			method :getSomeNum do
-				4
-			end
-			method :getAnotherNum do
-				3
-			end
-		end
-
+		helperGeneric2(:getSomeNum, 6, 4, :getAnotherNum, 5, 3)
 		class SimpleTest
 			uses T1.solucionar_con(:getSomeNum, Proc.new {|a, b| Proc.new { |*args| b.call(args)}}).
 				solucionar_con(:getAnotherNum) {|a, b| Proc.new { |*args| a.call(args)}}  + T2
 		end
-
-		miTest = SimpleTest.new
-
-		expect(miTest.getSomeNum).to eq 4 #mediante proc
-		expect(miTest.getAnotherNum).to eq 5 #mediante bloque
-
-		
+		expect(SimpleTest.new.getSomeNum).to eq 4 #mediante proc
+		expect(SimpleTest.new.getAnotherNum).to eq 5 #mediante bloque
 		class TestSuma
 			uses T1.sumar(T2) {|a, b| Proc.new { |*args| a.call(args)}}
 		end
